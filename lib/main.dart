865dc01f09db7001b96a649d410603749339a787
basim <bbasimsalah@gmail.com>
2021-11-20 00:39:36 +0200

    B.Salah

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'login/Login.dart';
+void main() {
+
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Login(),));
+}
+
+
+
+
---
+++ lib/model/productResponse.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:finalproject/model/product.dart';
+
+
+
+ProductResponse productResponseFromJson(String str) => ProductResponse.fromJson(json.decode(str));
+
+String productResponseToJson(ProductResponse data) => json.encode(data.toJson());
+
+class ProductResponse {
+  ProductResponse({
+    this.products,
+  });
+
+  List<Product> products;
+
+  factory ProductResponse.fromJson(Map<String, dynamic> json) => ProductResponse(
+    products: List<Product>.from(json["products"].map((x) => Product.fromJson(x))),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "products": List<dynamic>.from(products.map((x) => x.toJson())),
+  };
+}
+
